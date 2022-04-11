f51875249194bdbbce88abe4b4282e9713902348
Anna <anna.atsulus@gmail.com>
2022-04-11 14:01:27 +0300

    changes

--- newscript.js
+++ newscript.js
@@ -5,3 +5,4 @@ document.body.innerHTML = '<h1>say hi, java!</h1>'
 document.body.innerHTML = '<h1>say hi, java!</h1>'
 document.body.innerHTML = '<h1>say hi, java!</h1>'
 document.body.innerHTML = '<h1>say hi, java!</h1>'
+
