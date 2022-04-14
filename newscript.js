f8d7792e9be20c353242822746dff7332e183a6d
Anna <anna.atsulus@gmail.com>
2022-04-14 14:42:30 +0300

    tet

--- newscript.js
+++ newscript.js
@@ -1,8 +1,1 @@
-document.body.innerHTML = '<h1>say hi, java!</h1>'
-
-sdasfdsgdf
-document.body.innerHTML = '<h1>say hi, java!</h1>'
-document.body.innerHTML = '<h1>say hi, java!</h1>'
-document.body.innerHTML = '<h1>say hi, java!</h1>'
-document.body.innerHTML = '<h1>say hi, java!</h1>'
 
