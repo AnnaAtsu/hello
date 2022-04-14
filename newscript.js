90a76d7cb17b8634fb136b8d383893fd9bd2aee4
Anna <anna.atsulus@gmail.com>
2022-04-14 15:23:28 +0300

    javascript2

--- newscript.js
+++ newscript.js
@@ -1,7 +1,26 @@
 
-var number = 30;
+
 
 let Nu = 25; // актуальная переменная
 const BIRTH = "19.12.1995"; // ее не переопределишь
-console.log('Переменная равна '+ number);
 console.log('Переменная равна '+ Nu);
+
+let num = 5.4;
+let abc = Number('150');
+let c = num + abc;
+c++;
+console.log(c);
+
+Math.PI;
+Math.E;
+console.log(Math.min(4, -8, 6, 9));
+
+let number = 15;
+
+if(number == 5) {
+
+    console.log('it is five');
+}
+else {
+    console.log('it is  not five');
+}
